7087d4baedb519c8ba2e1ea47b5f134e13127765
ruby94412 <ruby94412@gmail.com>
2022-11-24 12:43:46 +0800

    MBD-37 refactor front end support

--- src/services/deviceServices.js
+++ src/services/deviceServices.js
@@ -5,7 +5,7 @@ const deviceServices = {
   getDeviceList: data => http.post('/device/getDeviceList', data, {headers: authHeader()}),
   addDevice: data => http.post('/device/add', data, {headers: authHeader()}),
   updateDevice: data => http.post('/device/update', data, {headers: authHeader()}),
-  deleteDevice: data => http.delete('/device/delete', data, {headers: authHeader()}),
+  deleteDevice: data => http.post('/device/delete', data, {headers: authHeader()}),
 }
 
 export default deviceServices;
